6f0838597d8bcc26fcf20eada70496adc26b1ed8
kenny <kennycs@protonmail.com>
2025-05-09 22:20:52 -0400

    Initial commit

---
+++ app/src/main/java/cx/glean/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package cx.glean.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Honey = Color(0xFFFFC30B)
+val PowderBlue = Color(0xFF6D91FF)
+val Pink = Color(0xFFDD688A)
+
+val LightHoney = Color(0xFFFFDB6D)
+val LightPowderBlue = Color(0xFF9FB7FF)
+val LightPink = Color(0xFFEFB8C8)
