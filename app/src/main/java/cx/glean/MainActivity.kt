2f3c7054a2df42f91cf8f7f90a9cf283d6caeb8f
kenny <kennycs@protonmail.com>
2025-05-10 17:22:22 -0400

    Added scaffold preview and bottom bar

--- app/src/main/java/cx/glean/MainActivity.kt
+++ app/src/main/java/cx/glean/MainActivity.kt
@@ -15,21 +15,37 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cx.glean.ui.theme.GleanTheme
 
+// TODO: adaptive layouts by screen size
+// TODO: reorganizing into different files
+// TODO: learn how much I should nest composables in main project
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,14 +53,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             GleanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GlimpseGrid(
-                        glimpses = listOf(),
-                        modifier = Modifier
-                            .consumeWindowInsets(innerPadding),
-                        contentPadding = innerPadding
-                    )
-                }
+                GlimpseScaffold(Modifier, listOf())
             }
         }
     }
@@ -59,8 +68,8 @@ fun GlimpseGrid(glimpses: List<Glimpse>, modifier: Modifier, contentPadding: Pad
         columns = GridCells.Adaptive(100.dp),
         modifier = modifier,
         contentPadding = contentPadding,
-        horizontalArrangement = Arrangement.spacedBy(5.dp),
-        verticalArrangement = Arrangement.spacedBy(5.dp)
+        horizontalArrangement = Arrangement.spacedBy(7.dp),
+        verticalArrangement = Arrangement.spacedBy(7.dp)
     ) {
         items(glimpses) {
             GlimpseCard(
@@ -90,19 +99,67 @@ fun GlimpseCard(glimpse: Glimpse, modifier: Modifier) {
             Text(
                 text = glimpse.author,
                 style = MaterialTheme.typography.labelLarge,
+                fontWeight = FontWeight.ExtraBold,
                 color = Color.White,
                 modifier = Modifier
                     .align(Alignment.BottomStart)
-                    .padding(4.dp)
+                    .padding(6.dp)
             )
         }
     }
 }
 
+@Composable
+fun GlimpseScaffold(modifier: Modifier, glimpses: List<Glimpse>) {
+    Scaffold(
+        modifier = modifier
+            .fillMaxSize(),
+        bottomBar = {
+            GleanBottomBar(modifier)
+        }
+    ) { innerPadding ->
+        GlimpseGrid(
+            glimpses = glimpses,
+            modifier = Modifier
+                .consumeWindowInsets(innerPadding)
+                .padding(7.dp),
+            contentPadding = innerPadding
+        )
+    }
+}
+
+@Composable
+fun GleanBottomBar(modifier: Modifier) {
+    var selectedItem by remember { mutableIntStateOf(1) }
+
+    var items = listOf("Create", "View", "Info", "Settings")
+    
+    NavigationBar(
+        modifier = modifier,
+    ) {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = {
+                    when (index) {
+                        0 -> Icon(Icons.Filled.AddCircle, contentDescription = null)
+                        1 -> Icon(Icons.Filled.Star, contentDescription = null)
+                        2 -> Icon(Icons.Filled.Info, contentDescription = null)
+                        3 -> Icon(Icons.Filled.Settings, contentDescription = null)
+                    }
+                },
+                // TODO: ???
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }
+            )
+        }
+    }
+}
+
+// TODO: fix glimpse card preview. scale card size to screen size
 @Preview
 @Composable
 fun PreviewGlimpseCard() {
-    var glimpse = Glimpse("Marisol", 387, R.drawable.preview_2, R.string.preview_2_content_description)
+    var glimpse = previewGlimpses[6]
     GlimpseCard(
         glimpse = glimpse,
         modifier = Modifier
@@ -112,23 +169,31 @@ fun PreviewGlimpseCard() {
 @Preview
 @Composable
 fun PreviewGlimpseGrid() {
-    var glimpses = listOf(
-        Glimpse("Elena", 557, R.drawable.preview_1, R.string.preview_1_content_description),
-        Glimpse("Marisol", 387, R.drawable.preview_2, R.string.preview_2_content_description),
-        Glimpse("Kristy", 407, R.drawable.preview_3, R.string.preview_3_content_description),
-        Glimpse("Virgie", 553, R.drawable.preview_4, R.string.preview_4_content_description),
-        Glimpse("Buford", 416, R.drawable.preview_5, R.string.preview_5_content_description),
-        Glimpse("Liz", 330, R.drawable.preview_6, R.string.preview_6_content_description),
-        Glimpse("Ariel", 327, R.drawable.preview_7, R.string.preview_7_content_description),
-        Glimpse("Noelle", 441, R.drawable.preview_8, R.string.preview_8_content_description),
-        Glimpse("Marion", 440, R.drawable.preview_9, R.string.preview_9_content_description),
-        Glimpse("Rosie", 328, R.drawable.preview_10, R.string.preview_10_content_description),
-        Glimpse("Hoyt", 419, R.drawable.preview_11, R.string.preview_11_content_description)
-    )
-
     GlimpseGrid(
-        glimpses = glimpses,
+        glimpses = previewGlimpses,
         modifier = Modifier,
         contentPadding = PaddingValues(0.dp)
     )
 }
+
+@Preview
+@Composable
+fun PreviewScaffold() {
+    GleanTheme {
+        GlimpseScaffold(Modifier, previewGlimpses)
+    }
+}
+
+var previewGlimpses = listOf(
+    Glimpse("Elena", 557, R.drawable.preview_1, R.string.preview_1_content_description),
+    Glimpse("Marisol", 387, R.drawable.preview_2, R.string.preview_2_content_description),
+    Glimpse("Kristy", 407, R.drawable.preview_3, R.string.preview_3_content_description),
+    Glimpse("Virgie", 553, R.drawable.preview_4, R.string.preview_4_content_description),
+    Glimpse("Buford", 416, R.drawable.preview_5, R.string.preview_5_content_description),
+    Glimpse("Liz", 330, R.drawable.preview_6, R.string.preview_6_content_description),
+    Glimpse("Ariel", 327, R.drawable.preview_7, R.string.preview_7_content_description),
+    Glimpse("Noelle", 441, R.drawable.preview_8, R.string.preview_8_content_description),
+    Glimpse("Marion", 440, R.drawable.preview_9, R.string.preview_9_content_description),
+    Glimpse("Rosie", 328, R.drawable.preview_10, R.string.preview_10_content_description),
+    Glimpse("Hoyt", 419, R.drawable.preview_11, R.string.preview_11_content_description)
+)
